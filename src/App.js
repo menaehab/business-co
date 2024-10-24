d2ba78f6e2ed64915a8fb35bdc74ccb667c89561
menaehab <commanderyt123@gmail.com>
2024-10-24 15:36:19 +0300

    Setup about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Offer from "./components/Offer";
 import "./App.css";
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <Offer />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style/about.css";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+export default function About() {
+  return (
+    <section className="about">
+      <Container>
+        <div className="about_text">
+          <div className="about_icon">
+            <i className="fas fa-anchor"></i>
+          </div>
+          <h1>About Company</h1>
+          <p>
+            Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur id suscipit ex. Suspendisse rhoncus laoreet purus quis
+            elementum. Phasellus sed efficitur dolor, et ultricies sapien.
+            Quisque fringilla sit amet dolor commodo efficitur. Aliquam et sem
+            odio. In ullamcorper nisi nunc, et molestie ipsum iaculis sit.
+          </p>
+          <Link className="btn_about">our work</Link>
+        </div>
+      </Container>
+    </section>
+  );
+}
