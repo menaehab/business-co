5515558b793e6baf3b5013d79ea92cfa28eb9912
menaehab <commanderyt123@gmail.com>
2024-10-25 20:23:29 +0300

    Implement Global Header and Footer for all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Offer from "../components/Offer";
 import About from "../components/About";
@@ -7,12 +6,10 @@ import Service from "../components/Service";
 import Engagement from "../components/Engagement";
 import Communication from "../components/Communication";
 import Market from "../components/Market";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <Offer />
       <About />
@@ -20,7 +17,6 @@ export default function Home() {
       <Service />
       <Communication />
       <Market />
-      <Footer />
     </>
   );
 }
